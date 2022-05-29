495f5f2308a367a8aae897a138ee385d1c1b3190
Joseph <josewsq101@gmail.com>
2022-05-29 16:17:39 -0300

    Creacion de Clases

---
+++ TP1POO/TP1POO/TransportePublico.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1POO
+{
+    public abstract class TransportePublico
+    {
+        public int Pasajeros { get; set; }
+        public string Nombre { get; set; }             
+        public TransportePublico(string nombre, int pasajeros)
+        {
+            this.Nombre = nombre;
+            this.Pasajeros = pasajeros;
+        }
+        public abstract int Devolver();
+        public abstract string Avanzar();
+        public abstract string Detenerse();
+    }
+}
