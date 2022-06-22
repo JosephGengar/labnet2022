5bd192212e5582be0842648ef6a90c02740e6800
Joseph <josewsq101@gmail.com>
2022-06-22 16:05:03 -0300

    API solicitudes POST insert and update

--- MVC/Entity.MVC.API/Controllers/ShippersController.cs
+++ MVC/Entity.MVC.API/Controllers/ShippersController.cs
@@ -37,6 +37,7 @@ namespace Entity.MVC.API.Controllers
             }
             return Ok(oResponse);
         }
+
         //Delete https://localhost:xxxxx/api/Shippers/id
         [HttpDelete]
         public IHttpActionResult Delete(int id)
@@ -46,6 +47,70 @@ namespace Entity.MVC.API.Controllers
             {
                 oShippersL.Delete(id);
                 oResponse.exito = 1;
+                oResponse.message = "Shipper Deleted";
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                {
+                    oShippersL.LogicDelete(id);
+                    oResponse.exito = 1;
+                    oResponse.message = "Error with delete operation, however the system looked for a way to solve it";
+                }
+                else
+                {
+                    oResponse.exito = 0;
+                    oResponse.message = ex.Message;
+                }              
+            }
+            return Ok(oResponse);
+        }
+
+        //{                               {
+        //"CompanyName" : "string",       "ShipperID" : int,
+        //"PhoneNumber" : "string"        "CompanyName" : "string", 
+        //}                               "PhoneNumber" : "string   }
+        [HttpPost]
+        public IHttpActionResult AddEdit(ShippersView model)
+        {
+            ResponseBack oResponse = new ResponseBack();
+            try
+            {
+                if (model.ShipperID != 0)
+                {
+                    if (!ModelState.IsValid)
+                    {
+                        return BadRequest(ModelState);
+                    }
+                    else
+                    {
+                        Shippers oShippers = new Shippers
+                        {
+                            ShipperID = model.ShipperID,
+                            CompanyName = model.CompanyName,
+                            Phone = model.PhoneNumber
+                        };
+                        oShippersL.Edit(oShippers);
+                        oResponse.exito = 1;
+                        oResponse.message = "Shipper Edited";
+                    }
+                }
+                else
+                {
+                    if (!ModelState.IsValid)
+                    {
+                        return BadRequest(ModelState);
+                    }
+                    else
+                    {
+                        Shippers oShippers = new Shippers();
+                        oShippers.CompanyName = model.CompanyName;
+                        oShippers.Phone = model.PhoneNumber;
+                        oShippersL.Add(oShippers);
+                        oResponse.exito = 1;
+                        oResponse.message = "Saved Shipper";
+                    }
+                }
             }
             catch (Exception ex)
             {
