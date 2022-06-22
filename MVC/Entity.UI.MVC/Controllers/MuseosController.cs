e7367aa32fa56c4210a72931367da07e18713d52
Joseph <josewsq101@gmail.com>
2022-06-22 15:31:18 -0300

    Consumiendo APIs pusheos

---
+++ MVC/Entity.UI.MVC/Controllers/MuseosController.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Entity.UI.MVC.Controllers
+{
+    public class MuseosController : Controller
+    {
+        // GET: Museos
+        public async Task<ActionResult> Index()
+        {
+            var httpClientApi = new HttpClient();
+            var jsonEntry = await httpClientApi.GetStringAsync("https://www.cultura.gob.ar/api/v2.0/museos/");
+            var museos = JsonConvert.DeserializeObject(jsonEntry);
+            return View(museos);
+        }
+    }
+}
